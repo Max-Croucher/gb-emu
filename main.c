f5b1fb36cd4dff39234cdaf2079dedd4e64bdd94
Max Croucher <mcr93@uclive.ac.nz>
2025-05-15 14:54:54 +1200

    implemented calls and prefixed opcodes

--- main.c
+++ main.c
@@ -141,7 +141,7 @@ uint8_t block00(gbRom* rom, uint8_t* ram, Registers* reg, uint8_t opcode) {
         offset_pc = 1;
         *(ram+get_r16(reg, (opcode>>4)&3)) = get_r8(reg, R8A);
         break;
-    case 10: //LD A, [r16] | load byte pointed to by address [r16] into LDA
+    case 10: //LD A, [r16] | load byte pointed to by address [r16] into A
         offset_pc = 1;
         set_r8(reg, R8A, *(ram+get_r16(reg, (opcode>>4)&3)));
         break;
@@ -308,6 +308,98 @@ uint8_t block10(gbRom* rom, uint8_t* ram, Registers* reg, uint8_t opcode) {
     }
     return offset_pc;
 }
+
+
+uint8_t prefixCB(gbRom* rom, uint8_t* ram, Registers* reg, uint8_t opcode) {
+    /* execute a 0xCB prefixed opcode */
+
+    uint8_t offset_pc = 0;
+    uint16_t working16bit;
+    uint16_t imm16;
+    uint8_t working8bit;
+    uint8_t imm8;
+    bool workingflag;
+    uint8_t has_finished = 1;
+    switch (opcode>>3)
+    {
+    case 0b00000: //RLC r8 | bit-shift rotate register r8 left, store wraparound into C
+        offset_pc = 1;
+        set_flag(reg, CFLAG, get_r8(reg, (opcode&7))>>7);
+        working8bit = (get_r8(reg, (opcode&7)) << 1) + get_flag(reg, CFLAG);
+        set_r8(reg, (opcode&7), working8bit);
+        set_flag(reg, ZFLAG, 0);
+        set_flag(reg, HFLAG, 0);
+        set_flag(reg, NFLAG, 0);
+        break;
+    case 0b00001: //RRC r8 | bit-shift rotate register r8 right, store wraparound into C
+        offset_pc = 1;
+        set_flag(reg, CFLAG, get_r8(reg, (opcode&7))&1);
+        working8bit = (get_r8(reg, (opcode&7)) >> 1) + (get_flag(reg, CFLAG)<<7);
+        set_r8(reg, (opcode&7), working8bit);
+        set_flag(reg, ZFLAG, 0);
+        set_flag(reg, HFLAG, 0);
+        set_flag(reg, NFLAG, 0);
+        break;
+    case 0b00010: //RL r8 | bit-shift rotate register r8 left, through C
+        offset_pc = 1;
+        workingflag = get_flag(reg, CFLAG);
+        set_flag(reg, CFLAG, get_r8(reg, (opcode&7))>>7);
+        working8bit = (get_r8(reg, (opcode&7)) << 1) + workingflag;
+        set_r8(reg, (opcode&7), working8bit);
+        set_flag(reg, ZFLAG, 0);
+        set_flag(reg, HFLAG, 0);
+        set_flag(reg, NFLAG, 0);
+        break;
+    case 0b00011: //RR r8 | bit-shift rotate register r8 right, through C
+        offset_pc = 1;
+        workingflag = get_flag(reg, CFLAG);
+        set_flag(reg, CFLAG, get_r8(reg, (opcode&7))&1);
+        working8bit = (get_r8(reg, (opcode&7)) >> 1) + (workingflag<<7);
+        set_r8(reg, (opcode&7), working8bit);
+        set_flag(reg, ZFLAG, 0);
+        set_flag(reg, HFLAG, 0);
+        set_flag(reg, NFLAG, 0);
+        break;
+    case 0b00100: //SLA r8 | bit-shit arithmetically register r8 left, into C
+        offset_pc = 1;
+        set_flag(reg, CFLAG, get_r8(reg, (opcode&7)) >> 7);
+        set_r8(reg, (opcode&7), get_r8(reg, (opcode&7))<<1);
+        set_flag(reg, ZFLAG, get_r8(reg, (opcode&7))==0);
+        set_flag(reg, HFLAG, 0);
+        set_flag(reg, NFLAG, 0);
+        break;
+    case 0b00101: //SRA r8 | bit-shit arithmetically register r8 right, into C
+        offset_pc = 1;
+        set_flag(reg, CFLAG, get_r8(reg, (opcode&7))&1);
+        set_r8(reg, (opcode&7), get_r8(reg, (opcode&7))>>1 + get_r8(reg, (opcode&7))&128);
+        set_flag(reg, ZFLAG, get_r8(reg, (opcode&7))==0);
+        set_flag(reg, HFLAG, 0);
+        set_flag(reg, NFLAG, 0);
+        break;
+    case 0b00110: //SWAP r8 | swap upper 4 and lower 4 bits in register r8
+        offset_pc = 1;
+        imm8 = get_r8(reg, (opcode&7)) << 4;
+        set_r8(reg, (opcode&7), get_r8(reg, imm8 + (opcode&7)) >> 4);
+        break;
+    case 0b00111: //SRL r8 | bit-shift logically register r8 right, into C
+        offset_pc = 1;
+        set_flag(reg, CFLAG, get_r8(reg, (opcode&7))&1);
+        set_r8(reg, (opcode&7), get_r8(reg, (opcode&7))>>1);
+        set_flag(reg, ZFLAG, get_r8(reg, (opcode&7))==0);
+        set_flag(reg, HFLAG, 0);
+        set_flag(reg, NFLAG, 0);
+        break;
+    default:;
+        bool workingflag;
+        char buf[80];
+        sprintf(buf, "Unknown 0xCB prefixed opcode at PC=0x%.4x OPCODE=0x%.2x BITS=%d%d%d%d%d%d%d%d",
+            (*reg).PC,opcode,(opcode>>7)&1,(opcode>>6)&1,(opcode>>5)&1,(opcode>>4)&1,
+            (opcode>>3)&1,(opcode>>2)&1,(opcode>>1)&1,(opcode>>0)&1);
+        print_error(buf);
+    }
+    return offset_pc;
+}
+
 
 uint8_t block11(gbRom* rom, uint8_t* ram, Registers* reg, uint8_t opcode) {
     /* execute an instruction for an opcode beginning with 0b11 */
@@ -482,7 +574,84 @@ uint8_t block11(gbRom* rom, uint8_t* ram, Registers* reg, uint8_t opcode) {
         memcpy(ram+(*reg).SP, &((*reg).PC), 2);
         set_r16(reg, R16PC, working16bit);
         break;
-
+    case 0b11000001:
+    case 0b11010001:
+    case 0b11100001:
+    case 0b11110001: //POP r16stk | pop register from stack into r16stk
+        offset_pc = 1;
+        memcpy(&working16bit, ram+(*reg).SP, 2);
+        (*reg).SP+=2;
+        switch ((opcode>>4)&3)
+        {
+        case 0:
+            (*reg).BC = working16bit;
+            break;
+        case 1:
+            (*reg).DE = working16bit;
+            break;
+        case 2:
+            (*reg).HL = working16bit;
+            break;
+        case 3:
+            (*reg).AF = working16bit;
+            break;
+        }
+        break;
+    case 0b11000101:
+    case 0b11010101:
+    case 0b11100101:
+    case 0b11110101: //PUSH r16stk | push register from r16stk into stack
+        offset_pc = 1;
+        switch ((opcode>>4)&3)
+        {
+        case 0:
+            working16bit = (*reg).BC;
+            break;
+        case 1:
+            working16bit = (*reg).DE;
+            break;
+        case 2:
+            working16bit = (*reg).HL;
+            break;
+        case 3:
+            working16bit = (*reg).AF;
+            break;
+        }
+        (*reg).SP-=2;
+        memcpy(ram+(*reg).SP, &working16bit, 2);
+        break;
+    case 0b11001011: // All prefixed 0xCB opcodes
+        offset_pc = 1;
+        offset_pc += prefixCB(rom, ram, reg, *(&((*reg).PC)+1));
+        break;
+    case 0b11100010: // LDH [c], A | load the byte in A to [0xFF00+C] 
+        offset_pc = 1;
+        *(ram+0xFF+get_r8(reg, R8C)) = get_r8(reg, R8A);
+        break;
+    case 0b11100000: // LDH [imm8], A | load the byte in A to [0xFF00+imm8] 
+        offset_pc = 2;
+        memcpy(&imm8, ram+(*reg).PC+1, 1);
+        *(ram+0xFF+imm8) = get_r8(reg, R8A);
+        break;
+    case 0b11101010: // LD [imm16], A | load the byte in A to [imm16] 
+        offset_pc = 3;
+        memcpy(&imm16, ram+(*reg).PC+1, 2);
+        *(ram+imm16) = get_r8(reg, R8A);
+        break;
+    case 0b11110010: // LDH A, [c] | load the byte in [0xFF00+C] to A 
+        offset_pc = 1;
+        set_r8(reg, R8A, *(ram+0xFF+get_r8(reg, R8C)));
+        break;
+    case 0b11110000: // LDH A, [imm8] | load the byte in [0xFF00+imm8] to A 
+        offset_pc = 2;
+        memcpy(&imm8, ram+(*reg).PC+1, 1);
+        set_r8(reg, R8A, *(ram+0xFF+imm8));
+        break;
+    case 0b11111010: // LD A, [imm16] | load the byte in [imm16] to A
+        offset_pc = 3;
+        memcpy(&imm16, ram+(*reg).PC+1, 2);
+        set_r8(reg, R8A, *(ram+imm16));
+        break;
     default:;
         bool workingflag;
         char buf[64];
@@ -493,6 +662,7 @@ uint8_t block11(gbRom* rom, uint8_t* ram, Registers* reg, uint8_t opcode) {
     }
     return offset_pc;
 }
+
 
 void run_instruction(gbRom* rom, uint8_t* ram, Registers* reg) {
     /* read the opcode at the PC and execute an instruction */
@@ -526,7 +696,27 @@ int main(int argc, char *argv[]) {
     uint8_t *ram = init_ram(&rom);
     Registers reg = init_registers();
 
-    *(ram+(reg).PC) = 0b11111111;
+
+    set_r16(&reg, R16BC, 0x1234);
+    set_r16(&reg, R16DE, 0x89ab);
+    set_r16(&reg, R16HL, 0xeffe);
+    *(ram+(reg).PC) = 0b11000101; //push BC
+    *(ram+(reg).PC+1) = 0b11010101; //push DE
+    *(ram+(reg).PC+2) = 0b11100101; //push HL
+    *(ram+(reg).PC+3) = 0b00000001; //Load to BC
+    *(ram+(reg).PC+4) = 0b00000000; //0x0000
+    *(ram+(reg).PC+5) = 0b00000000;
+    *(ram+(reg).PC+6) = 0b00010001; //Load to DE
+    *(ram+(reg).PC+7) = 0b00000000; //0x0000
+    *(ram+(reg).PC+8) = 0b00000000;
+    *(ram+(reg).PC+9) = 0b00100001; //Load to HL
+    *(ram+(reg).PC+10) = 0b00000000; //0x0000
+    *(ram+(reg).PC+11) = 0b00000000;
+    *(ram+(reg).PC+12) = 0b11000001; //Pop BC
+    *(ram+(reg).PC+13) = 0b11010001; //Pop DE
+    *(ram+(reg).PC+14) = 0b11100001; //Pop HL
+    *(ram+(reg).PC+15) = 0b00000000; //NOP
+    
 
     print_registers(&reg);
     for (int i=0; i<10; i++) {
