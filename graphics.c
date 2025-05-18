ad3dc20e3d83febfce8d3917678486e10e69aec2
Max Croucher <mcr93@uclive.ac.nz>
2025-05-18 17:46:23 +1200

    added the ability to print tilemaps to stdout

--- graphics.c
+++ graphics.c
@@ -8,8 +8,10 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdbool.h>
 #include <GL/freeglut.h>
 #include <time.h>
+#include "cpu.h"
 #include "graphics.h"
 
 clock_t start,end;
@@ -22,6 +24,7 @@ uint8_t xoffset = 0;
 uint32_t dot = 0;
 
 GLubyte texture[144][160][3];
+ObjectAttribute objects[10];
 
 void init_screen_tex(GLubyte texture[144][160][3]) {
     /* initialise the screen texture */
@@ -97,16 +100,75 @@ void init_graphics(int *argc, char *argv[]) {
 }
 
 
+uint16_t interleave(uint16_t a, uint16_t b) {
+    /* interleave the lower 8 bits in a and b, with the bits in b coming first
+        eg: a = 0000000012345678
+            b = 00000000abcdefgh
+        becomes a1b2c3d4e5f6g7h8
+    */
+    uint16_t a1 = ((a &0x00F0)<<4) + (a &0x000F); // shift first half (000000012345678 -> 0000123400005678)
+    uint16_t a2 = ((a1&0x0C0C)<<2) + (a1&0x0303); // shift first and third pair (0000123400005678 -> 0012003400560078)
+    uint16_t a3 = ((a2&0x2222)<<1) + (a2&0x1111); // shift odd bits (0012003400560078 -> 0102030405060708)
+    
+    uint16_t b1 = ((b &0x00F0)<<4) + (b &0x000F); // as above, with b
+    uint16_t b2 = ((b1&0x0C0C)<<2) + (b1&0x0303);
+    uint16_t b3 = ((b2&0x2222)<<2) + ((b2&0x1111)<<1); // one bit further over
+
+    return a3 + b3;
+}
+
+
+void load_tile(uint8_t* ram, uint16_t tile_data[8], uint8_t tile_id, bool is_object) {
+    /* Read RAM to produce an 8x8 tile (stored in tile_data) */
+    uint16_t tile_addr = 0x8000 + (uint16_t)(tile_id)*16;
+    if (!(is_object) && tile_id < 128 && !(*(ram+0xFF40)&8)) tile_addr += 0x1000; // swap addressing mode if tile isn't an object
+    for (int i=0; i<8; i++) {
+        tile_data[i] = interleave(*(ram+tile_addr+(2*i)), *(ram+tile_addr+(2*i+1)));
+    }
+}
+
+
+uint8_t read_objects(uint8_t* ram, ObjectAttribute attrbank[10], uint8_t scanline) {
+    /* builds an array of up to 10 object attributes that intersect with the current scanline */
+    uint8_t objects_found = 0;
+    uint8_t offset_scanline = scanline + 16;
+    bool tile8x8 = *(ram+0xFF40) & (1<<2); //1 if 8x8, 0 if 8x16
+    for (int i=0; i<40; i++) {
+        if (objects_found == 10) break;
+        uint8_t ypos = *(ram+0xFFE0+(i*4));
+        if (
+            (tile8x8 && ypos+8>offset_scanline) && //8x8 sprite isn't above scanline
+            (!tile8x8 && ypos+16>offset_scanline) && //8x16 isn't above scanline
+            (ypos<=offset_scanline) //sprite isn't below scanline
+        ) { //Object intersects scanline
+            uint8_t flags = *(ram+0xFFE0+(i*4)+3);
+            attrbank[objects_found] = (ObjectAttribute){
+                .ypos = ypos,
+                .xpos = *(ram+0xFFE0+(i*4)+1),
+                .tileid = *(ram+0xFFE0+(i*4)+2),
+                .priority = flags & (1<<7),
+                .yflip = flags & (1<<7),
+                .xflip = flags & (1<<7),
+                .palette = flags & (1<<7)
+            };
+            objects_found++;
+        }
+    }
+    return objects_found;
+}
+
+
 void tick_graphics(uint8_t *ram) {
     /* Main tick procedure for graphics */
-    
-    dot++;
-    if (dot == 70224) {
-        dot = 0;
+    uint8_t scanline = dot/456;
+    if (dot == 65564) { // enter VBLANK
+        set_render_blocking_mode(1);
+        *(ram+0xFF0F) |= 1; // Request a VBlank interrupt
         xoffset++;
         if (xoffset == 144) xoffset = 0;
-        glutMainLoopEvent();
-        glutPostRedisplay();
+        //glutMainLoopEvent();
+        //glutPostRedisplay();
+        //print_tilemaps(ram);
         end = clock();
         frametime += ((double)(end-start)) / CLOCKS_PER_SEC;
         framecount_offset++;
@@ -115,7 +177,45 @@ void tick_graphics(uint8_t *ram) {
             fprintf(stderr, "framerate: %.2fHz\n", 1.0/(frametime/FRAMETIME_BUFSIZE));
             frametime = 0;
         }
-        printf("framerate: %.2fHz\n", 1.0/(frametime/10));
+        fprintf(stderr,"framerate: %.2fHz\n", 1.0/(frametime/10));
         start = clock();
+    } else if ((scanline < 144) && (dot % 456) == 0) { // New scanline
+        set_render_blocking_mode(2);
+        ObjectAttribute objects[10];
+        uint8_t objects_found = read_objects(ram, objects, scanline);
+
+    } else if ((scanline < 144) && (dot % 456) == 80) { // Enter drawing mode
+        set_render_blocking_mode(3);
+
+    } else if ((scanline < 144) && (dot % 456) == 252) { // Enter Hblank
+        set_render_blocking_mode(0);
+    }
+
+
+
+
+
+    dot++;
+    if (dot == 70224) {
+        dot = 0;
+    }
+}
+
+
+void print_tilemaps(uint8_t *ram) {
+    for (int i=0; i<32; i++) {
+        uint16_t tiles[32][8];
+        char print_palette[4] = {' ', '.', 'o', '0'};
+        for (int j=0; j<32; j++) {
+            load_tile(ram, tiles[j], *(ram+0x9800+(i*32)+j), 0);
+        }
+        for (int k=0; k<8; k++) {
+            for (int j=0; j<32; j++) {
+                for (int l=7; l>=0; l--){
+                    printf("%c", print_palette[(tiles[j][k]>>(2*l))&3]);
+                }
+            }
+            printf("\n");
+        }
     }
 }
--- graphics.h
+++ graphics.h
@@ -7,11 +7,25 @@
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
 
+typedef struct {
+    uint8_t ypos;
+    uint8_t xpos;
+    uint8_t tileid;
+    bool priority;
+    bool yflip;
+    bool xflip;
+    bool palette;
+} ObjectAttribute;
+
 void init_screen_tex(GLubyte texture[144][160][3]);
 void gl_tick(void);
 void reshape(int w, int h);
 void init_graphics(int *argc, char *argv[]);
+uint16_t interleave(uint16_t a, uint16_t b);
+void load_tile(uint8_t* ram, uint16_t tile_data[8], uint8_t tile_id, bool is_object);
+uint8_t read_objects(uint8_t* ram, ObjectAttribute attrbank[10], uint8_t scanline);
 void tick_graphics(uint8_t *ram);
+void print_tilemaps(uint8_t *ram);
 
 
 
